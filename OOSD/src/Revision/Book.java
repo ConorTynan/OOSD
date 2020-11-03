8a2af898963efd1c1a33f3c23c6e06652e130cb2
tynan <tynan@DESKTOP-R5TP1HH>
2020-11-03 14:08:32 +0000

    .

---
+++ OOSD/src/Revision/Book.java
@@ -0,0 +1,111 @@
+package Revision;
+
+public class Book 
+{
+	///Variables
+	private String title;
+	private String author;
+	private int noOfPages;
+	private int edition;
+	private int chapters;
+	private char status;
+	
+	///Constructors
+	public Book(String title, String author, int noOfPages, int edition, int chapters) 
+	{
+		setTitle(title);
+		setAuthor(author);
+		setNoOfPages(noOfPages);
+		setEdition(edition);
+		setChapters(chapters);
+		setStatus('A');
+	}
+	public Book(String title, String author, int noOfPages, int edition) 
+	{
+		setTitle(title);
+		setAuthor(author);
+		setNoOfPages(noOfPages);
+		setEdition(edition);
+		setStatus('A');
+	}
+	public Book(String title, String author, int noOfPages) 
+	{
+		setTitle(title);
+		setAuthor(author);
+		setNoOfPages(noOfPages);
+		setStatus('A');
+	}
+
+	///Getters & Setters
+	public String getTitle() 
+	{
+		return title;
+	}
+	public void setTitle(String title) 
+	{
+		this.title = title;
+	}
+	public String getAuthor() 
+	{
+		return author;
+	}
+	public void setAuthor(String author) 
+	{
+		this.author = author;
+	}
+	public int getNoOfPages() 
+	{
+		return noOfPages;
+	}
+	public void setNoOfPages(int noOfPages) 
+	{
+		this.noOfPages = noOfPages;
+	}
+	public int getEdition() 
+	{
+		return edition;
+	}
+	public void setEdition(int edition) 
+	{
+		this.edition = edition;
+	}
+	public int getChapters() 
+	{
+		return chapters;
+	}
+	public void setChapters(int chapters) 
+	{
+		if(chapters>0&&chapters<10)
+		{
+			this.chapters = chapters;
+		}
+		else
+		{
+			System.out.println("Chapters not in range.");
+		}
+	}
+	public char getStatus()
+	{
+		return status;
+	}
+	public void setStatus(char status) 
+	{
+		this.status = status;
+	}
+	public void borrow()
+	{
+		setStatus('B');
+	}
+	public void BookReturn()
+	{
+		setStatus('A');
+	}
+	
+	///toString
+	@Override
+	public String toString() 
+	{
+		return "Book [title=" + title + ", author=" + author + ", noOfPages=" + noOfPages + ", edition=" + edition
+				+ ", chapters=" + chapters + ", status=" + status + "]";
+	}
+}
---
+++ OOSD/src/Revision/Library.java
@@ -0,0 +1,38 @@
+package Revision;
+
+import java.util.*;
+public class Library 
+{
+	public static void main(String[] args)
+	{
+		Book[] books=new Book[3];
+	
+		for (int i=0;i<=2;i++)
+		{
+			Scanner input=new Scanner(System.in);
+			System.out.println("Enter title:");
+			String title=input.nextLine();
+			System.out.println("Enter author:");
+			String author=input.nextLine();
+			System.out.println("Enter no. of pages:");
+			int noOfPages=input.nextInt();
+		
+			Book b=new Book(title, author, noOfPages);
+			books[i]=b;
+		}
+		for(Book b:books)
+		{
+			if(b!=null)System.out.println(b.toString());
+		}
+		books[1].borrow();
+		for(Book b:books)
+		{
+			if(b!=null)System.out.println(b.toString());
+		}
+		books[1].BookReturn();
+		for(Book b:books)
+		{
+			if(b!=null)System.out.println(b.toString());
+		}
+	}
+}
