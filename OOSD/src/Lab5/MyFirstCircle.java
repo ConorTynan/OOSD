8cdde09449ba44600a08d05fb9e6f48f489d7d6f
tynan <tynan@DESKTOP-R5TP1HH>
2020-11-18 10:24:36 +0000

    Lab5

--- OOSD/src/Lab5/MyFirstCircle.java
+++ OOSD/src/Lab5/MyFirstCircle.java
@@ -6,6 +6,7 @@ public class MyFirstCircle
 	{
 		Point myPoint = new Point(10, 20);
 		Circle myCircle = new Circle(15, 30, 5);
+		
 		System.out.println("Point details : " + myPoint);
 		System.out.println("Circle details: " + myCircle);
 	}
