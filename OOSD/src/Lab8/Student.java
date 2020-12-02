933378d5df48c9401e05d8076391ea09db4b76ca
tynan <tynan@DESKTOP-R5TP1HH.station>
2020-12-02 11:43:45 +0000

    Lab8

---
+++ OOSD/src/Lab8/Student.java
@@ -0,0 +1,29 @@
+package Lab8;
+
+public class Student extends Person 
+{
+	String course;
+	
+	public Student(String name, String course)
+	{
+		super(name);
+		setCourse(course);
+	}
+
+	@Override
+	public String getDescription() 
+	{
+		return "A Student studying " + course;
+	}
+
+	public String getCourse() 
+	{
+		return course;
+	}
+	public void setCourse(String course) 
+	{
+		this.course = course;
+	}
+	
+
+}
